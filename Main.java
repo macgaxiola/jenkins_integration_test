5dc41e3a3fd3ab4deb7a28375dbf83d5a2bd19a7
Mac <test@test>
2020-06-14 19:19:54 -0500

    Change to trigger CI

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
 	public static void main(String [] args){
 		System.out.println("Testing CI with Jenkins and java code");  
+		System.out.println("Adding changes to trigger CI");
 	}
 }
